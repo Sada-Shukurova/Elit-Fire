4191bd4f1a475b4380a87b6b62d61569be74f92c
sada shukurova <shukurova.sada@gmail.com>
2023-08-07 14:38:19 +0400

    add js file and swiper

---
+++ app.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    autoplay: {
+    },
+    speed: 1600,
+    slidesPerView: 1,
+});
